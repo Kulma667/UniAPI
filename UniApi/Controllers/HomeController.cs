dded18d7043880f76128fb9c6f4d3c70c007c9fe
Kulma667 <timurkzalmaty@gmail.com>
2023-09-19 13:32:57 +0600

    UniApi proccessing for a CRUD by http requests from others api

---
+++ UniApi/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UniApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Welcoem to User API";
+        }
+
+        [HttpPost]
+        public string Index([FromBody] Dictionary<string , string> InoputData)
+        { 
+            return "";
+        }
+        public string GetUsers()
+        {
+            return "";
+        }
+        public string GetUser(string id) 
+        {
+            return "";
+        }
+        public string CreateUser(string Id, string Name, string Email, string Password, bool Sex) 
+        {
+            return "";
+        }
+        public string ChangePassword(string Id, string Email, string oldPassword) 
+        {
+            if (oldPassword != "")
+            {
+                return "Try again";
+            }
+            return "Succsess";
+        }
+    }
+}
---
+++ UniApi/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace UniApi.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        private string Password { get; set; }
+        public bool Sex { get; set; }
+
+    }
+}
