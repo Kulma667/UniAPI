3c32127690fd65b131dd69fd80362ae71876faef
Kulma667 <timurkzalmaty@gmail.com>
2023-09-19 14:01:59 +0600

    зададки для verify пароля

--- UniApi/Controllers/HomeController.cs
+++ UniApi/Controllers/HomeController.cs
@@ -1,17 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft;
+using Newtonsoft.Json;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace UniApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]/[action]")]
     [ApiController]
     public class HomeController : ControllerBase
     {
         [HttpGet]
         public string Index()
         {
-            return "Welcoem to User API";
+            return "Welcome to User API";
         }
 
         [HttpPost]
@@ -38,6 +42,25 @@ namespace UniApi.Controllers
                 return "Try again";
             }
             return "Succsess";
+        }
+        public string hasher256(string Password, string Key) 
+        {
+            string resulthash = ""; 
+            Key = "";
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(Password + Key);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+                string hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                resulthash = hashString;
+            }
+            return JsonConvert.SerializeObject(resulthash);
+        }
+        public string VerifyHash(string hashString) 
+        {
+
+            string originalString = ""; //получать праоль из базы и бить ее в хешер для верифая
+            return JsonConvert.SerializeObject(originalString);
         }
     }
 }
